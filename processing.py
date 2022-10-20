5423570a13936d91b3c2d68b0b1969ea9b583204
solowiejmaciej <55091850+solowiejmaciej@users.noreply.github.com>
2022-10-20 21:36:09 +0200

    Update processing.py

--- processing.py
+++ processing.py
@@ -10,7 +10,7 @@ import ssl
 import certifi
 from aiohttp.client import ClientSession
 
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+#asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 CHROME = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
